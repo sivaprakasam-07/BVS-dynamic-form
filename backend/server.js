55bc180ae9173aef7163be16613929307d00172e
sivaprakasam-07 <tamilmanisivaprakasam5@gmail.com>
2025-04-13 23:19:20 +0530

    created new branch main

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,7 @@ app.use(bodyParser.json());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
 })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log('MongoDB connection error:', err));
